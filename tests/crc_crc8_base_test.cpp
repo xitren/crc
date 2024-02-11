48612bcb00b4c882a25726bf86db14bcd2f5c8cf
xitren <xitren@ya.ru>
2024-02-11 15:24:40 +0300

    Test fixed

--- tests/crc_crc8_base_test.cpp
+++ tests/crc_crc8_base_test.cpp
@@ -7,11 +7,11 @@ using namespace xitren::crc;
 TEST(crc8_test, calc_match)
 {
     std::array<std::uint8_t, 5> arr{1, 2, 3, 4, 5};
-    EXPECT_EQ(crc8(arr.begin(), arr.end()), 0xbc);
+    EXPECT_EQ(crc8::calculate(arr.begin(), arr.end()), 0xbc);
 }
 
 TEST(crc8_test, stm32_id)
 {
     std::array<std::uint8_t, 12> id{0x75, 0x37, 0x53, 0x46, 0x4a, 0x35, 0x47, 0x16, 0x56, 0x54, 0x38, 0x42};
-    EXPECT_EQ(crc8(id.begin(), id.end()), 0x80);
+    EXPECT_EQ(crc8::calculate(id.begin(), id.end()), 0x80);
 }
